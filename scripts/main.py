6a98f091aa2663c0ee3b3cb393cec88b02a01162
MichalMotylinski <motylm66@gmail.com>
2023-03-11 20:02:19 +0000

    Added new properties. Added decimation and clip to the code. Testing if loading xtf is correct. Added sliders for the new properties.

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
-from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QSlider, QLabel, QLineEdit
+from PyQt6.QtCore import pyqtSlot, Qt
 import sys
 import os
 import pyXTF
@@ -12,32 +12,97 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         
-        self.setGeometry(200, 200, 300, 300)
+        self.setGeometry(200, 200, 600, 600)
         self.setWindowTitle("SSS")
-        self.initUI()
+        
 
         self.filepath = None
         self.filename = None
+        self.port_data = None
+        self.starboard_data = None
+
+        self._decimation = 4
+        self._clip = 1.0
+        self._stretch = None
+        self._color_scheme = None
+        self._invert = False
+        
+        self.initUI()
+
+    @property
+    def decimation(self):
+        """The decimation property."""
+        return self._decimation
+    
+    @decimation.setter
+    def decimation(self, val):
+        self._decimation = val
+
+    @property
+    def clip(self):
+        """The clip property."""
+        return self._clip
+    
+    @clip.setter
+    def clip(self, val):
+        self._clip = val
 
     def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("LAbel")
-        self.label.move(50,50)
+        self.label_decimation = QLabel(self)
+        self.label_decimation.move(10, 5)
+        self.label_decimation.setText(f"Decimation: {self.decimation}")
+        self.label_decimation.adjustSize()
+
+        self.slider_decimation = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider_decimation.setGeometry(10, 15, 100, 40)
+        self.slider_decimation.setMinimum(1)
+        self.slider_decimation.setMaximum(10)
+        self.slider_decimation.setValue(self.decimation)
+        self.slider_decimation.setTickInterval(1)
+        self.slider_decimation.valueChanged.connect(self.update_decimation)
+
+        self.label_clip = QLabel(self)
+        self.label_clip.move(150, 5)
+        self.label_clip.setText(f"Clip: {self.clip}")
+        self.label_clip.adjustSize()
+
+        self.slider_clip = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider_clip.setGeometry(150, 15, 100, 40)
+        self.slider_clip.setMinimum(0)
+        self.slider_clip.setMaximum(100)
+        self.slider_clip.setValue(self.clip * 100)
+        self.slider_clip.setTickInterval(1)
+        self.slider_clip.valueChanged.connect(self.update_clip)
 
         self.open_file_btn = QPushButton(self)
         self.open_file_btn.setText("Open file dialog")
-        
-
+        self.open_file_btn.move(450, 0)
         self.open_file_btn.clicked.connect(self.open_dialog)
-
 
         self.b = QtWidgets.QPushButton(self)
         self.b.setText("Clickme")
         self.b.clicked.connect(self.read_xtf)
         self.b.move(100, 100)
 
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Clickme")
+        self.b1.clicked.connect(self.clicked)
+        self.b1.move(400, 400)
+
+    def update_decimation(self):
+        self.decimation = self.sender().value()
+        self.label_decimation.setText(f"Decimation: {str(self.sender().value())}")
+        self.label_decimation.adjustSize()
+    
+    def update_clip(self):
+        self.clip = self.sender().value() / 100
+        self.label_clip.setText(f"Clip: {str(self.sender().value() / 100)}")
+        self.label_clip.adjustSize()
+    
+
     def clicked(self):
-        print(self.filename, self.filepath)
+        print(self.filename, self.filepath, self.decimation, self._decimation)
+        print(np.array(self.port_data).shape, np.array(self.starboard_data).shape)
         self.label.setText("you pressed the button")
         self.update()
 
@@ -61,8 +126,7 @@ class MyWindow(QMainWindow):
         channelB = 1
 
         maxSamplesPort, maxSamplesStbd, minAltitude, maxAltitude, maxSlantRange, pingCount, meanSpeed, navigation = getSampleRange(self.filepath, channelA, channelB, True)
-    
-        acrossTrackSampleInterval = (maxSlantRange / maxSamplesPort) # sample interval in metres
+        acrossTrackSampleInterval = (maxSlantRange / maxSamplesPort) * self.decimation # sample interval in metres
         
         # to make the image somewhat isometric, we need to compute the alongtrack sample interval.  this is based on the ping times, number of pings and mean speed  where distance = speed * duration
         distance = 1#meanSpeed * (navigation[-1].dateTime.timestamp() - navigation[0].dateTime.timestamp())
@@ -81,7 +145,7 @@ class MyWindow(QMainWindow):
             if ping == -999:
                 continue
 
-            channel = np.array(ping.pingChannel[0].data)
+            channel = np.array(ping.pingChannel[0].data[::self.decimation])
             channel = np.multiply(channel, math.pow(2, -ping.pingChannel[0].Weight))
             
             filteredPortData = channel.tolist()
@@ -89,12 +153,14 @@ class MyWindow(QMainWindow):
             for i in range(stretch):
                 pc.insert(0, filteredPortData[::-1])
            
-            channel = np.array(ping.pingChannel[1].data)
+            channel = np.array(ping.pingChannel[1].data[::self.decimation])
             channel = np.multiply(channel, math.pow(2, -ping.pingChannel[1].Weight))
             rawStbdData = channel.tolist()
             for i in range(stretch):
                 sc.insert(0, rawStbdData)
         
+        self.port_data = np.array(pc)
+        self.starboard_data = np.array(sc)
         print(np.array(pc).shape, np.array(sc).shape)
 
 
@@ -137,6 +203,7 @@ def window():
     
 
     win.show()
+
     sys.exit(app.exec())
 
 window()
